880a495478c184ca5d7ca2fc0d97bdd861192e00
Tushar Raj <153943708+ATUSHARRAJ@users.noreply.github.com>
2025-12-16 01:00:14 +0530

    Change GitHub login route to GET with callback

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ const { protect } = require('../middleware/authMiddleware');
 const { googleLogin, githubLogin, login , register} = require("../controllers/signup");
 // Define the GET route
 router.post("/auth/google-login", googleLogin);
-router.post("/auth/github-login", githubLogin);
+router.get("/auth/github/callback", githubLogin);
 router.post("/auth/login", login);
 router.post("/auth/register", register);
 router.get('/all-problems', getAllProblems);
